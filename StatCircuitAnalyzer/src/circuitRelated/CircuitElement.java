e495300c85982553427959d8e45d6ee70c2a7d4c
sarahsong7 <jysong@se.kaist.ac.kr>
2017-02-21 10:10:05 +0900

    change Element.java to CircuitElement.java

---
+++ StatCircuitAnalyzer/src/circuitRelated/CircuitElement.java
@@ -0,0 +1,43 @@
+package circuitRelated;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import plcopen.inf.type.group.fbd.IBlock;
+import plcopen.inf.type.group.fbd.IInVariable;
+import plcopen.inf.type.group.fbd.IOutVariable;
+
+public class CircuitElement {
+	public static final int INVAR = 1;
+	public static final int BLOCK = 2;
+	public static final int OUTVAR = 3;
+
+	public static final int BOOLEAN = 1;
+	public static final int INTEGER = 2;
+	public static final int REAL = 3;
+
+	public String FormalParam;
+
+	public int type;
+	public long LocalID;
+
+	public IInVariable invar;
+	public IBlock block;
+	public IOutVariable outvar;
+
+	public List<CircuitElement> prevElement;
+	public List<CircuitElement> nextElement;
+
+	public String value;
+	public int valueType;
+
+	public boolean constant = false;
+
+	public CircuitElement(int type, long LocalID) {
+		this.type = type;
+		this.LocalID = LocalID;
+		
+		prevElement = new ArrayList<CircuitElement>();
+		nextElement = new ArrayList<CircuitElement>();
+	}
+}
